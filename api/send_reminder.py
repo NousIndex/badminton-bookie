6b6550cc9df7db00d963ab8a0d1045cb2092d4e2
XiangHui <92633730+XiangHui556@users.noreply.github.com>
2025-02-07 13:27:05 +0800

    Update 8

--- api/send_reminder.py
+++ api/send_reminder.py
@@ -61,6 +61,6 @@ def home():
 @app.get("/send_reminder")
 async def manual_trigger(request: Request):
     headers = dict(request.headers)
-    print(headers.auth_key)
+    print(headers)
     #await send_reminder()
     return {"message": "Reminder sent!", "headers": headers}
