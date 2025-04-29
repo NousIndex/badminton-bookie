0319fb44f3ab9cafc5561e3202e94a61e3a762c0
XiangHui <92633730+XiangHui556@users.noreply.github.com>
2025-04-29 19:33:32 +0800

    Update send_reminder.py
    Delete message next day

--- api/send_reminder.py
+++ api/send_reminder.py
@@ -1,5 +1,6 @@
 import os
 import telegram
+from telegram.ext import (ContextTypes)
 from fastapi import FastAPI, Request
 from pytz import timezone
 from datetime import datetime, timedelta
@@ -8,6 +9,7 @@ import hashlib
 import base64
 from typing import Dict
 import time
+from pymongo import MongoClient
 
 app = FastAPI()
 
@@ -16,15 +18,47 @@ CHAT_ID = os.getenv("TELE_GROUP_CHAT_ID")
 CHAT_ID2 = os.getenv("TELE_GROUP_CHAT_ID2")
 AUTH_KEY = os.getenv("AUTH_KEY_DECODER")
 KEY_WORD = os.getenv("KEY_WORD")
+MONGODB_URI = os.getenv("MONGODB")
+
+client = MongoClient(MONGODB_URI)
+db = client["BadmintonBookie"]
+collection = db["DeleteMessages"]
+
+sg_timezone = timezone("Asia/Singapore")
 
 today_date = datetime.today()
 bot = telegram.Bot(token=TOKEN)
 
-sg_timezone = timezone("Asia/Singapore")
-
+now_sgt = datetime.now(sg_timezone)
+future_date_plus1 = now_sgt + timedelta(days=1)
+current_date_str = now_sgt.strftime("%Y-%m-%d")
+future_date_plus1_str = future_date_plus1.strftime("%Y-%m-%d")
 
 def unpad(s):
     return s[: -ord(s[-1])]
+
+def save_message(chat_id: str, message_id: str):
+    collection.insert_one({
+        "Date": future_date_plus1_str,
+        "ChatId": str(chat_id),
+        "MessageId": str(message_id)
+    })
+    
+async def delete_message_today():
+    result = collection.find({"Date": current_date_str})
+    for doc in result:
+        print(doc)
+        try:
+            await bot.delete_message(
+                chat_id=doc['ChatId'],
+                message_id=doc['MessageId']
+            )
+            collection.delete_one({
+                "ChatId": doc['ChatId'],
+                "MessageId": doc['MessageId']
+            })
+        except Exception as e:
+            print(f"Failed to delete message: {e}")
 
 
 def aes_decrypt(encrypted_text, key):
@@ -49,7 +83,7 @@ async def send_reminder():
     timestamp_3pm = int(future_date_3pm.timestamp()) * 1000
     timestamp_4pm = int(future_date_4pm.timestamp()) * 1000
 
-    await bot.send_message(
+    msg = await bot.send_message(
         chat_id=CHAT_ID,
         text="Ballot Reminder:\nhttps://activesg.gov.sg/venues/WYfbYK8b8mvlTx7iiCIJp/activities/YLONatwvqJfikKOmB5N9U/review/ballot"
         + "?timeslot="
@@ -64,6 +98,7 @@ async def send_reminder():
         # + str(timestamp_4pm),
         disable_notification=True,
     )
+    save_message(msg.chat.id, msg.message_id)
 
 async def send_reminder2():
     future_date = today_date + timedelta(days=14)
@@ -80,7 +115,7 @@ async def send_reminder2():
     timestamp_8pm = int(future_date_8pm.timestamp()) * 1000
     timestamp_9pm = int(future_date_9pm.timestamp()) * 1000
 
-    await bot.send_message(
+    msg = await bot.send_message(
         chat_id=CHAT_ID2,
         text="Ballot Reminder:\nhttps://activesg.gov.sg/venues/a3jznoZlsfyJrl43Tbnog/activities/YLONatwvqJfikKOmB5N9U/review/ballot"
         + "?timeslot="
@@ -95,9 +130,10 @@ async def send_reminder2():
         # + str(timestamp_4pm),
         disable_notification=True,
     )
+    save_message(msg.chat.id, msg.message_id)
 
 async def court_place(courtdate, location, timeslot, court):
-    await bot.send_message(
+    msg = await bot.send_message(
         chat_id=CHAT_ID,
         text="Court Reminder For Tomorrow:\n📍: "
         + location
@@ -109,6 +145,7 @@ async def court_place(courtdate, location, timeslot, court):
         + court,
         disable_notification=True,
     )
+    save_message(msg.chat.id, msg.message_id)
 
 
 @app.get("/")
@@ -119,7 +156,7 @@ def home():
 @app.get("/send_reminder")
 async def manual_trigger(request: Request):
     headers = dict(request.headers)
-    print(headers)
+    # print(headers)
     try:
         if aes_decrypt(headers["auth_key"], AUTH_KEY) == KEY_WORD:
             await send_reminder()
@@ -132,7 +169,7 @@ async def manual_trigger(request: Request):
 @app.get("/send_reminder_work")
 async def manual_trigger(request: Request):
     headers = dict(request.headers)
-    print(headers)
+    # print(headers)
     try:
         if aes_decrypt(headers["auth_key"], AUTH_KEY) == KEY_WORD:
             await send_reminder2()
@@ -142,6 +179,18 @@ async def manual_trigger(request: Request):
         print("FAILED EXCEPT")
     return {"message": "Reminder sent!"}
 
+@app.get("/delete_message_today")
+async def manual_trigger3(request: Request):
+    headers = dict(request.headers)
+    # print(headers)
+    try:
+        if aes_decrypt(headers["auth_key"], AUTH_KEY) == KEY_WORD:
+            await delete_message_today()
+        else:
+            print("FAILED")
+    except:
+        print("FAILED EXCEPT")
+    return {"message": "Reminder sent!"}
 
 @app.post("/send_reminder_court")
 async def manual_trigger2(request: Request):
